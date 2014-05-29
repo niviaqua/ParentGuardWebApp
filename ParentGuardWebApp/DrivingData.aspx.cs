2bd77f74161d0b5e484b4ab63281d1dab4c953c2
Nivedita Joshi <niviaqua@gmail.com>
2014-05-28 22:13:49 -0400

    SQLData

--- ParentGuardWebApp/DrivingData.aspx.cs
+++ ParentGuardWebApp/DrivingData.aspx.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Data.Sql;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Configuration;
+using System.Data.SqlClient;
 
 namespace ParentGuardWebApp
 {
@@ -13,15 +16,32 @@ namespace ParentGuardWebApp
         {
             System.IO.StreamReader reader = new System.IO.StreamReader(HttpContext.Current.Request.InputStream);
             string requestFromPost = reader.ReadToEnd();
-            
-            //loop through
-            string formValue;
-            if (!string.IsNullOrEmpty(Request.Form["txtFormValue"]))
-            {
-                formValue = Request.Form["txtFormValue"];
-                formValue = Request.Form["txtImei"];
 
+            //loop through
+           // string formValue;
+
+            string speed;
+            string initialLocation;
+            string finalLocation;
+            string IMEI;
+
+            if (!string.IsNullOrEmpty(Request.Form["txtSpeed"]))
+            {
+                //formValue = Request.Form["txtSpeed"];
+                //formValue = Request.Form["txtImei"];
+                speed = Request.Form["Speed"];
+                initialLocation = Request.Form["initialLocation"];
+                finalLocation = Request.Form["finalLocation"];
+                IMEI = Request.Form["IMEI"];
+
+                string s = ConfigurationManager.ConnectionStrings["conn"].ConnectionString;
+                SqlConnection cn = new SqlConnection(s);
+                cn.Open();
+                SqlCommand cmd = new SqlCommand("insert into DataHistory(Speed, initialLocation, finalLocation, IMEI)values('" + speed + "','" + initialLocation + "','" + finalLocation + "','" + IMEI + "')", cn);
+                cmd.ExecuteNonQuery();
+	
             }
+
         }
     }
 }
